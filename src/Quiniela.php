ed5a43382eed1fb3528740c8bab42e0529d56095
PabloOjer <ojer.161782@e.unavarra.es>
2026-06-15 13:04:50 +0200

    [verde] - Añadir array que guarda varias apuestas

--- src/Quiniela.php
+++ src/Quiniela.php
@@ -4,12 +4,15 @@ namespace Deg540\CleanCodeKata9;
 
 class Quiniela
 {
+    private $apuestas = "";
     public function apostar(string $apuesta): string
     {
         $equipos = explode(" ", $apuesta)[1];
         $ganador = explode(" ", $apuesta)[2];
-        if(in_array($ganador,["1","2","x","X"]))
-            return $equipos . ": " . $ganador;
+        if(in_array($ganador,["1","2","x","X"])){
+            $this->apuestas = $this->apuestas . $equipos . ": " . $ganador . ", ";
+            return substr($this->apuestas, 0, -2);
+        }
         return "Signo no valido";
     }
 }
